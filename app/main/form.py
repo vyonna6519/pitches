849daff9f0cb773242bcf3cb61b216268ea1af2f
vyonna <vyonna.njenga@student.moringaschool.com>
2022-03-06 16:19:23 +0300

    imported DataRequired

--- app/main/form.py
+++ app/main/form.py
@@ -1,18 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, TextAreaField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Write a brief bio about you.',validators = [Required()])
+    bio = TextAreaField('Write a brief bio about you.',validators = [DataRequired()])
     submit = SubmitField('Save')
 
 class PitchForm(FlaskForm):
-    title = StringField('Title', validators=[Required()])
-    category = SelectField('Category', choices=[('Events','Events'),('Job','Job'),('Advertisement','Advertisement')],validators=[Required()])
-    post = TextAreaField('Your Pitch', validators=[Required()])
+    title = StringField('Title', validators=[DataRequired()])
+    category = SelectField('Category', choices=[('Events','Events'),('Job','Job'),('Advertisement','Advertisement')],validators=[DataRequired()])
+    post = TextAreaField('Your Pitch', validators=[DataRequired()])
     submit = SubmitField('Pitch')
 
 class CommentForm(FlaskForm):
-    comment = TextAreaField('Leave a comment',validators=[Required()])
+    comment = TextAreaField('Leave a comment',validators=[DataRequired()])
     submit = SubmitField('Comment')
 
