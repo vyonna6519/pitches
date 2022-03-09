a69acb1be076d6c6c9ba3fd065b92d18da897498
vyonna <vyonna.njenga@student.moringaschool.com>
2022-03-09 17:56:45 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -17,6 +17,8 @@ class Config:
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:vyonna6519@localhost/one_min_pitch'
+
     DATABASE_URL="postgresql://klsunfjvzymaje:e1cbc0b16cc1c7220244939f14054179bdbeb62fbaf906ce8aa1a4e5e0a3c933@ec2-52-45-83-163.compute-1.amazonaws.com:5432/dh90kbtvo3kid"
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:vyonna6519@localhost/one_min_pitch'
 
@@ -31,13 +33,13 @@ class Config:
 class ProdConfig(Config):
     # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     # pass
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-       SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
-    pass 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    #    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:vyonna6519@localhost/one_min_pitch'
 
     DEBUG = True
 
