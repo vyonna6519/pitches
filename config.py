78473fd1325e3a04719b43316bacb797fe36cea9
vyonna <vyonna.njenga@student.moringaschool.com>
2022-03-09 11:06:29 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config:
     
 
 class ProdConfig(Config):
-   
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass
 
 class DevConfig(Config):
---
+++ start.sh
@@ -0,0 +1,7 @@
+export SECRET_KEY='562af35a9b244efba09ef9e1a482d134x9'
+
+export MAIL_USERNAME="vyonna.njenga@student.moringaschool.com"
+export MAIL_PASSWORD="moringa6519"
+
+
+python3.8 manage.py 
